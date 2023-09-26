573ddecd8b5d869bbd1544e6f0995140b29437e6
Vamshi Peddi <139543424+VamshiPeddi17@users.noreply.github.com>
2023-09-26 13:50:25 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Check the valid Identifiers and invalid identifers using Python
+
+# 1. **Run all the identifiers and seprate valid and Invalid Identifiers?**
+# 
+# 2. **Why Python treating them as invalid Identifers.Explain?**
+
+# * my_variable = 25
+# 
+# **Valid
+# 
+# 
+# * counter = 40.7
+# 
+# **Valid
+# 
+# 
+# * _total = "Hello"
+# 
+# **Valid
+# 
+# 
+# - 123abc   = 34   
+# 
+# **Invalid (As it is starting with number/digit, it only starts with Alphabet or underscore)
+# 
+#  
+#  - my variable   =6.7
+#  
+# **Invalid  (As it is having a space in the identifier)
+# 
+#  
+#  - @count  = "hi"     
+#  **Invalid (As it is starting with a symbol (@)/only underscore is accepted)
+#  
+#  
+#  - for   = "True"
+#  **Invalid (Keyword is used as identifier which is not possible)
+#  
+#  
+# * Name123 = 136
+# **Valid
+# 
+# * myFunction = True
+# 
+# **Valid
+# 
+# * _data = False
+# 
+# **Valid
+# 
+# - if_condition? = "Innomatics"
+# 
+# **Invalid  (As it is ending with a ?, it only allows underscore)
+# 
+# 
+# - class = "Hyderabad"
+# 
+# **Invalid  (As it is ending with a ?, it only allows underscore)
+# 
+# 
+
+# ### Use different identifiers and assign the below values
+
+# 10
+# 
+# -5
+# 
+# 3.14159
+# 
+# -0.12345
+# 
+# 1000
+# 
+# "Hello, World!"
+# 
+# "Python"
+# 
+# "I am learning"
+# 
+# "Text type"
+# 
+# "String"
+# 
+# True
+# 
+# False
+# 
+# True
+# 
+# False
+# 
+# True
+# 
+# 
+
+# ### Assigning identifiers
+
+# In[82]:
+
+
+num = 10
+
+
+# In[95]:
+
+
+print(num)
+type(num)
+
+
+# In[84]:
+
+
+number2 = -5
+
+
+# In[94]:
+
+
+print(number2)
+type(number2)
+
+
+# In[86]:
+
+
+Float = 3.14159
+
+
+# In[93]:
+
+
+print(Float)
+type(Float)
+
+
+# In[89]:
+
+
+Float_negative = -0.12345
+
+
+# In[92]:
+
+
+print(Float_negative)
+type(Float_negative)
+
+
+# In[57]:
+
+
+my_num = 1000
+
+
+# In[91]:
+
+
+print(my_num)
+type(my_num)
+
+
+# In[59]:
+
+
+_first_string = "Hello, World!"
+
+
+# In[60]:
+
+
+print(_first_string)
+type(_first_string)
+
+
+# In[61]:
+
+
+___1 = "Python"
+
+
+# In[62]:
+
+
+print(___1)
+type(___1)
+
+
+# In[63]:
+
+
+YES_= "I am learning"
+
+
+# In[64]:
+
+
+print(YES_)
+type(YES_)
+
+
+# In[65]:
+
+
+_text = " Text type"
+
+
+# In[66]:
+
+
+print(_text)
+type(_text)
+
+
+# In[67]:
+
+
+Finally = "String"
+
+
+# In[68]:
+
+
+print(Finally)
+type(Finally)
+
+
+# In[71]:
+
+
+Truee = True
+
+
+# In[72]:
+
+
+Truee
+
+
+# In[73]:
+
+
+Falsee = False
+
+
+# In[74]:
+
+
+Falsee
+
+
+# In[75]:
+
+
+Bool = True
+
+
+# In[76]:
+
+
+Bool
+
+
+# In[77]:
+
+
+Boolean = False
+
+
+# In[78]:
+
+
+Boolean
+
+
+# In[79]:
+
+
+Tbool  = True
+
+
+# In[81]:
+
+
+Tbool
+
